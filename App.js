e719326d923ae6dbe357c5358529b08eec1472e4
GonzaloElia <gelia@udesa.edu.ar>
2022-11-12 16:39:14 -0300

    carperta expo

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
-export default function App() {
+function App() {
   return (
     <View>
       <Text>Hola mundo!</Text>
     </View>
   );
 }
+export default App
