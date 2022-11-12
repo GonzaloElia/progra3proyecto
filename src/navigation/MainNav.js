4075717e6de6fdd8ea08be6a67759c7b764abee9
GonzaloElia <gelia@udesa.edu.ar>
2022-11-12 18:24:41 -0300

    arreglo en mainNav

--- src/navigation/MainNav.js
+++ src/navigation/MainNav.js
@@ -1,5 +1,5 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, {Component} from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from '../screens/Login/Login'
